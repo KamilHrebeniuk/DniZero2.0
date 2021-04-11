a1a2823b631afc408e302edaa078fef9d942692f
Michał <michalgradzik1000@gmail.com>
2021-04-11 16:23:34 +0200

    Zobaczymy czy dziala

--- src/components/MyAccount/myAccount.js
+++ src/components/MyAccount/myAccount.js
@@ -1,7 +1,36 @@
 import React from "react";
 
-export default function MyAccount() {
+export default function MyAccount({name, guardian_picture, guardian_name, guardian_number}) {
     return (
-        <h1>MyAccount</h1>
+        <div className="account">
+            <div className="account-content">
+                <h1>Witaj, {name}</h1>
+                <h2>Twój opiekun:</h2>
+                <img className="guardian-picture" src={guardian_picture} alt={guardian_name}/>
+                <h2>{guardian_name}</h2>
+                <h3>Numer telefonu: {guardian_number}</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur . Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="account-menu">
+                        <button className="account-button">Lista moich warsztatów</button>
+                        <button className="account-button">Lista moich drużyn</button>
+                        <button className="account-button">Historia akcji</button>
+                        <button className="account-close_button">Zamknij</button>
+                </div>
+                <div className="account-footer">
+                    <p>Kliknij po za oknem by zamknąć</p>
+                </div>
+            </div>
+
+        </div>
+
     )
+
+
 }
+
---
+++ src/components/Popup/popup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Popup(){
+    return(
+        <div className="popup">
+            <div className="popup-content">
+
+                <div className="popup-menu">
+                </div>
+            </div>
+        </div>
+    )
+}
