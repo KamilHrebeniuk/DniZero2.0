69531eea084928e3114a055d0c165171b5adc660
Michał <michalgradzik1000@gmail.com>
2021-08-03 23:25:45 +0200

    #10 widok dla niezalogowanego plus CSS dla logowania

--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -3,7 +3,46 @@ import React from "react";
 export default function Register() {
   return (
     <div className="register">
-      <div className="register-content">Rejestracja!!!!</div>
+      <div className="register-content">
+        <p>Rejestracja</p>
+        <form>
+          <label className="required" htmlFor="name">
+            Imie:
+          </label>
+          <br />
+          <input type="text" id="name" name="name" required />
+          <br />
+
+          <label className="required" htmlFor="surname">
+            Nazwisko:
+          </label>
+          <br />
+          <input type="text" id="surname" name="surname" required />
+          <br />
+
+          <label className="required" htmlFor="mail">
+            Email:
+          </label>
+          <br />
+          <input type="email" id="mail" name="mail" required />
+          <br />
+
+          <label className="required" htmlFor="number">
+            Numer telefonu:
+          </label>
+          <br />
+          <input
+            type="tel"
+            pattern="[0-9]{9}"
+            id="number"
+            name="number"
+            required
+          />
+          <br />
+
+          <input type="submit" value="Wyślij!" />
+        </form>
+      </div>
     </div>
   );
 }
