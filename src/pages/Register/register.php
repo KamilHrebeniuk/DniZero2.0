98f12eb0a4a0fc68176b1f43d4fcaf15da7af462
Michał <michalgradzik1000@gmail.com>
2021-08-10 05:33:29 +0200

    #32 ulepszony PHP i js do kontaktu z baza danych

--- src/pages/Register/register.php
+++ src/pages/Register/register.php
@@ -3,40 +3,34 @@
 
     $data = file_get_contents("php://input");
     $DATA = json_decode($data,true);
-    $keys = ['name', 'email', 'phone', 'shirt', 'year', 'bus', 'diet', 'ICE_name', 'ICE_phone', 'ICE_adr', 'health', 'age', 'rules'];
-    $person = Array(
-        'name' => '',
-        'email' => '',
-        'phone' => '',
-        'shirt' => '',
-        'year' => '',
-        'bus' => '',
-        'diet' => '',
-        'ICE_name' => '',
-        'ICE_phone' => '',
-        'ICE_adr' => '',
-        'health' => '',
-        'age' => '',
-        'rules' => ''
+
+    $response= Array(
+        'result' => false,
+        'message' => '',
     );
-    foreach($keys as $key){
-        $person[$key]=$DATA[$key];
-    }
-    if($person){
+    if($DATA){
         $conn = Database::getConnection();
         try{
-            $stmt = $conn->prepare("INSERT INTO OBZ_persons (name, email, phone, shirt, year, bus, diet, ICE_name, ICE_phone, ICE_adr, health, age, rules) VALUES (".$person["name"].", ".$person["email"].", ".$person["phone"].", ".$person["shirt"].", ".$person["year"].", ".$person["bus"].", ".$person["diet"].", ".$person["ICE_name"].", ".$person["ICE_phone"].", ".$person["ICE_adr"].", ".$person["health"].", ".$person["age"].", ".$person["rules"]." )");
-            $to = $person["email"];
+            $stmt = $conn->prepare("INSERT INTO OBOZ_zapisy (id_osoby, name, email, phone, shirt, year, bus, diet, ICE_name, ICE_phone, ICE_adr, health, age_confirmation, rules_acceptance) VALUES (NULL,'".$DATA["name"]."', '".$DATA['email']."', '".$DATA['phone']."', '".$DATA['shirt']."', '".$DATA['year']."', '".$DATA['bus']."', '".$DATA['diet']."', '".$DATA['ICE_name']."', '".$DATA['ICE_phone']."', '".$DATA['ICE_adr']."', '".$DATA['health']."', '".$DATA['age']."', '".$DATA['rules']."' )");
+            $stmt->execute();
+            /*
+            $to = $DATA['email'];
             $subject = 'Oboz';
             $message = 'Dzialam';
             $headers = 'Content-Type: text/html; charset=utf-8'."\r\n".'From: oboz@oboz.pwr'."\r\n";
-            if(mail($to,$subject,$message,$headers)){
-                echo "To dopiero część skonćzonej przygody! Teraz wejdz na maila i postępuj zgodnie z instrukcjami!";
-            }else{
-                echo "Nie udało sie wysłać maila";
-            }
+            if(mail($to,$subject,$message,$headers)){ */
+                   $response['result'] = true;
+                   $response['message'] = "To dopiero część skonćzonej przygody! Teraz wejdz na maila i postępuj zgodnie z instrukcjami!";
+                   echo json_encode($response, JSON_UNESCAPED_UNICODE);
+ 	       /* }else{
+                $response['message'] = "Nie udało sie wysłać maila";
+ 	             echo json_encode($response, JSON_UNESCAPED_UNICODE);
+            }*/
         }catch (PDOException $e){
-            echo "Niepowodzenie! Rejestracja nie powiodła się: ".$e->getMessage();
+            $response['message'] = "Niepowodzenie! Rejestracja nie powiodła się: ".$e->getMessage();
+            echo json_encode($response,JSON_UNESCAPED_UNICODE);
+            $conn = null;
         }
-        $conn = null;
     }
+    $conn = null;
+?>
