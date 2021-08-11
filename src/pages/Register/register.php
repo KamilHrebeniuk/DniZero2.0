e4f7e1cea52af29524b12698d418f405ab3886f1
Michał <michalgradzik1000@gmail.com>
2021-08-11 14:54:29 +0200

    #11 zmiany w php

--- src/pages/Register/register.php
+++ src/pages/Register/register.php
@@ -4,7 +4,7 @@
 
     $data = file_get_contents("php://input");
     $DATA = json_decode($data,true);
-
+    $date = date("Y-m-d H:i:s");
     $response= Array(
         'result' => false,
         'message' => '',
@@ -13,26 +13,29 @@
         $conn = Database::getConnection();
         try{
             $stmt = $conn->prepare("INSERT INTO OBOZ_zapisy 
-            (id_osoby, name, email, phone, shirt, year, bus, diet, ICE_name, ICE_phone, ICE_adr, health, age_confirmation, rules_acceptance) 
-            VALUES (NULL, :name , :email , :phone , :shirt, :year, :bus, :diet, :ICE_name, :ICE_phone, :ICE_adr, :health, :age, :rules )");
+            (id_osoby, name, email, phone, pesel, adr, list_adr, shirt, year, bus, diet, ICE_name, ICE_phone, health, age_confirmation, rules_acceptance, date) 
+            VALUES (NULL, :name , :email , :phone, :pesel, :adr, :list_adr, :shirt, :year, :bus, :diet, :ICE_name, :ICE_phone, :health, :age, :rules , :date)");
             $stmt ->bindParam(':name',$DATA['name'],PDO::PARAM_STR);
             $stmt ->bindParam(':email',$DATA['email'],PDO::PARAM_STR);
             $stmt ->bindParam(':phone',$DATA['phone'],PDO::PARAM_STR);
+            $stmt->bindParam(':pesel',$DATA['pesel'],PDO::PARAM_STR);
+            $stmt->bindParam(':adr',$DATA['adr'],PDO::PARAM_STR);
+            $stmt->bindParam(':list_adr',$DATA['list_adr'],PDO::PARAM_STR);
             $stmt ->bindParam(':shirt',$DATA['shirt'],PDO::PARAM_STR);
             $stmt ->bindParam(':year',$DATA['year'],PDO::PARAM_INT);
             $stmt ->bindParam(':bus',$DATA['bus'],PDO::PARAM_STR);
             $stmt ->bindParam(':diet',$DATA['diet'],PDO::PARAM_STR);
             $stmt ->bindParam(':ICE_name',$DATA['ICE_name'],PDO::PARAM_STR);
             $stmt ->bindParam(':ICE_phone',$DATA['ICE_phone'],PDO::PARAM_STR);
-            $stmt ->bindParam(':ICE_adr',$DATA['ICE_adr'],PDO::PARAM_STR);
             $stmt ->bindParam(':health',$DATA['health'],PDO::PARAM_BOOL);
             $stmt ->bindParam(':age',$DATA['age'],PDO::PARAM_BOOL);
             $stmt ->bindParam(':rules',$DATA['rules'],PDO::PARAM_BOOL);
+            $stmt ->bindParam(':date',$date,PDO::PARAM_STR);
             $stmt->execute();
 
             $response['result'] = mailTo($DATA['email']);
             if($response['result']){
-                $response['message'] = "To dopiero część skonćzonej przygody! Teraz wejdz na maila i postępuj zgodnie z instrukcjami!";
+                $response['message'] = "To dopiero część skonćzonej przygody! Teraz wejdz na maila (sprawdź spam) i postępuj zgodnie z instrukcjami!";
             }else{
                 $response['message'] = "Nie udało sie wysłać maila";
             }
